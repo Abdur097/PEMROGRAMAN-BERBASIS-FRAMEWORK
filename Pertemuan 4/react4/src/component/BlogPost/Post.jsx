b4f0026aad9f0d1b6f87978e68dae4f5efe62a2a
Abdur097 <1941720094@student.polinema.ac.id>
2022-03-22 20:54:05 +0700

    Revisi praktikum

--- Pertemuan 4/react4/src/component/BlogPost/Post.jsx
+++ Pertemuan 4/react4/src/component/BlogPost/Post.jsx
@@ -8,10 +8,11 @@ const Post = (props) => {
             </div>
             <div className="konten-artikel">
                 <div className="judul-artikel">{props.judul}</div>
-                <p className="isi-artikel">{props.isi}</p>
-                 <button className="btn btn-sm btn-danger" onClick={() =>props.hapusArtikel(props.idArtikel)}>Hapus</button>
+                    <p className="isi-artikel">{props.isi}</p>
+                    <button className="btn btn-sm btn-warning" onClick={() => props.hapusArtikel(props.idArtikel)}>Hapus</button>
             </div>
         </div>
     )
 }
+
 export default Post;
