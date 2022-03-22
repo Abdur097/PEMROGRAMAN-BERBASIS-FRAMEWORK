dd884422da15ca882ceb44b648f12fdcca54a0f9
Abdur097 <1941720094@student.polinema.ac.id>
2022-03-22 20:52:35 +0700

    Revisi Praktikum

--- Pertemuan 4/react4/src/container/BlogPost/BlogPost.jsx
+++ Pertemuan 4/react4/src/container/BlogPost/BlogPost.jsx
@@ -2,68 +2,63 @@ import React, {Component} from "react";
 import './BlogPost.css';
 import Post from "../../component/BlogPost/Post";
 
-
-class BlogPost extends Component{
-    state = {               // komponen state dari React untuk statedull component
-        listArtikel : [],    // variable array yang digunakan untuk menyimpan data API
-        InsertArtikel : {
-            userId: 1,
+class BlogPost extends Component {
+    state = {                   // Komponen state dari React untuk statefull component
+        listArtikel: [],        // Variabel array yang digunakan untuk menyimpan data API
+        insertArtikel: {        // Variabel yang digunakan untuk menampung sementara data yang akan di insert
+            userId: 1,          // Kolom userId, id, title, dan body sama, mengikuti kolom yang ada pada listArtikel.json 
             id: 1,
-            title: '',
-            body: ''
+            title: "",
+            body: ""
         }
-    } 
+    }
 
-    ambilDataDariServerAPI = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts') // alamat URL API yang akan di ambil
-        .then(response => response.json()) // response dari API dalam bentuk JSON
-        .then(jsonHasilAmbilDariAPI => {
-            this.setState({
-                listArtikel: jsonHasilAmbilDariAPI
+    ambilDataDariServerAPI = () => {        // Fungsi untuk mengambil data dari API dengan penambahan sort dan order 
+        fetch(`http://localhost:3001/posts?_sort=id&_order=desc`)        // Penambahan sort dan order berdasarkan parameter
+            .then(response => response.json())      // Ubah response data dari URL API menjadi sebuah data json
+            .then(jsonHasilAmbilDariAPI => {        // Data json hasil ambil dari API kita masukkan ke dalam listArtikel pada state
+                this.setState({
+                    listArtikel: jsonHasilAmbilDariAPI
+                })
             })
-        })
     }
 
-    componentDidMount(){   // fungsi yang akan dijalankan ketika component telah di mount
-        this.ambilDataDariServerAPI() // ambil data dari server API lokal
+    componentDidMount() {           // Komponen untuk mengecek ketika component telah di-mount-ing, maka panggil API
+        this.ambilDataDariServerAPI()   // Ambil data dari server API lokal
     }
 
-    // handle delete button
-    handleHapusArtikel = (data) => { // fungsi yang akan di panggil ketika tombol hapus di klik
-        fetch(`http://localhost:3001/posts/${data}`, {method: 'delete'}) // alamat URL API yang akan di ambil
-        .then(res => { // response dari API dalam bentuk JSON
-            this.ambilDataDariServerAPI()
-        })
+    handleHapusArtikel = (data) => {        // Fungsi yang meng-handle button action hapus data
+        fetch(`http://localhost:3001/posts/${data}`, {method: 'DELETE'})        // Alamat URL API yang ingin kita HAPUS datanya
+            .then(res => {      // Ketika proses hapus berhasil, maka ambil data dari server API lokal
+                this.ambilDataDariServerAPI()
+            })
     }
 
-    // handle insert button
-    handleTambahArtikel = (event) => { // fungsi yang akan di panggil ketika tombol tambah di klik
-        let formInsertArtikel = {...this.state.InsertArtikel}; // mengambil state InsertArtikel dan di clone
-        let timestamp = new Date().getTime(); // digunakan untuk mengambil waktu saat ini
-        formInsertArtikel["id"] = timestamp; // mengisi field id dengan waktu saat ini
-        formInsertArtikel[event.target.name] = event.target.value; // mengisi field dengan properti name dan value
+    handleTambahArtikel = (event) => {          // Fungsi untuk meng-handle form tambah data artikel 
+        let formInsertArtikel = {...this.state.insertArtikel};      // Clonning data state insertArtikel ke dalam variabel formInsertArtikel
+        let timestamp = new Date().getTime();                       // Digunakan untuk menyimpan waktu (sebagai ID artikel)
+        formInsertArtikel['id'] = timestamp;
+        formInsertArtikel[event.target.name] = event.target.value;  // Menyimpan data onchane ke formInsertArtikel sesuai dengan target yang diisi
         this.setState({
-            InsertArtikel: formInsertArtikel
-        })
+            insertArtikel: formInsertArtikel
+        });
     }
 
-    // tombol simpan
-    handleTombolSimpan = () => {  //fungsi untuk menghandle tombol simpan
-        fetch('http://localhost:3001/posts', {
-            method: 'post',
-            headers: {  
+    handleTombolSimpan = () => {                                // Fungsi untuk meng-handle tombol simpan
+        fetch(`http://localhost:3001/posts`, {
+            method: 'post',                                     // Method POST untuk input/insert data
+            headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(this.state.InsertArtikel)
+            body: JSON.stringify(this.state.insertArtikel)      // Kirimkan ke body request untuk data artikel yang akan ditambahkan (insert)
         })
-        .then(res => { // response dari API dalam bentuk JSON
-            this.ambilDataDariServerAPI() //reload atau refresh data
-        })
+            .then( (response :Response ) => {
+                this.ambilDataDariServerAPI();                    // Reload / refresh data
+            });
     }
 
-    
-    render(){
+    render() {
         return(
             <div className="post-artikel">
                 <div className="form pb-2 border-bottom">
@@ -72,24 +67,28 @@ class BlogPost extends Component{
                         <div className="col-sm-10">
                             <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
                         </div>
-                        <div className="form-group row">
-                            <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
-                            <div className="col-sm-10">
-                                <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
-                            </div>
-                        </div>
-                        <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
                     </div>
+
+                    <div className="form-group row">
+                        <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
+                        <div className="col-sm-10">
+                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                        </div>
+                    </div>
+                
+                    <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
                 </div>
-                <h2> Daftar Artikel </h2>
+
+                <h2>Daftar Artikel</h2>
                 {
-                    this.state.listArtikel.map(artikel => { // looping data dari API
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} hapusArtikel={this.handleHapusArtikel} idArtikel={artikel.id}/> 
+                    this.state.listArtikel.map(artikel => {
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} 
+                        idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>
                     })
                 }
             </div>
-        );
+        )
     }
-
 }
+
 export default BlogPost;
