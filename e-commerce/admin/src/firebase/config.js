f4511d06534799b95c559c6dbd33721594499097
Abdur097 <74463635+Abdur097@users.noreply.github.com>
2022-06-15 18:07:03 +0700

    Add files via upload

---
+++ e-commerce/admin/src/firebase/config.js
@@ -0,0 +1,13 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZcQiT6HnUetwi9O-d8n95ohiy8K6TN6o",
+  authDomain: "tubespbf-7ce34.firebaseapp.com",
+  databaseURL: "https://tubespbf-7ce34-default-rtdb.firebaseio.com",
+  projectId: "tubespbf-7ce34",
+  storageBucket: "tubespbf-7ce34.appspot.com",
+  messagingSenderId: "788846758638",
+  appId: "1:788846758638:web:c4534e7316520cd1ae81ff",
+  measurementId: "G-3T55ZD4Y94"
+};
+  export default firebaseConfig
